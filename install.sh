14cdba606bc4f71d7b987ea0c237c57ecf64bfd6
Andrei Gorgan <aliasbind@gmail.com>
2012-11-02 21:56:32 +0200

    Add installer script for configs

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+INSTALLER_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# i3
+I3_CONFIG_DIR=~/.i3
+mkdir -p $I3_CONFIG_DIR
+ln -fs $INSTALLER_PATH/i3/config $I3_CONFIG_DIR/config
+
+# dunst
+DUNST_CONFIG_DIR=~/.config/dunst
+mkdir -p $DUNST_CONFIG_DIR
+ln -fs $INSTALLER_PATH/dunst/dunstrc $DUNST_CONFIG_DIR/dunstrc
