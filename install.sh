a69f8d9a135f8a60e7a72de296e58de8cdc8e53e
Andrei Gorgan <aliasbind@gmail.com>
2012-12-15 11:30:03 +0200

    Add vim configuration option in install.sh

--- install.sh
+++ install.sh
@@ -15,3 +15,18 @@ ln -fs $INSTALLER_PATH/dunst/dunstrc $DUNST_CONFIG_DIR/dunstrc
 # Xresources
 ln -fs $INSTALLER_PATH/Xresources ~/.Xresources
 xrdb ~/.Xresources
+
+# vim
+command -v git > /dev/null
+if [ $? -ne 0 ]
+then
+	sudo apt-get install git
+fi
+
+git clone git@github.com:aliasbind/vimconfig.git ~/.vim
+if [ $? -ne 0 ]
+then
+	echo "Failed to install vim configuration."
+	exit 1
+fi
+ln -s ~/.vim/vimrc ~/.vimrc
