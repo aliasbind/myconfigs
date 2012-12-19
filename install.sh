177daffe559184e85b0841d46ce9e350bf9cc874
Andrei Gorgan <aliasbind@gmail.com>
2012-12-19 09:20:55 +0200

    Make install script more verbose

--- install.sh
+++ install.sh
@@ -6,27 +6,38 @@ INSTALLER_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 I3_CONFIG_DIR=~/.i3
 mkdir -p $I3_CONFIG_DIR
 ln -fs $INSTALLER_PATH/i3/config $I3_CONFIG_DIR/config
+echo "i3: Success"
 
 # dunst
 DUNST_CONFIG_DIR=~/.config/dunst
 mkdir -p $DUNST_CONFIG_DIR
 ln -fs $INSTALLER_PATH/dunst/dunstrc $DUNST_CONFIG_DIR/dunstrc
+echo "dunst: Success"
 
 # Xresources
 ln -fs $INSTALLER_PATH/Xresources ~/.Xresources
 xrdb ~/.Xresources
+echo "Xresources: Success"
 
 # vim
-command -v git > /dev/null
-if [ $? -ne 0 ]
+if [ ! -d ~/.vim ]
 then
-	sudo apt-get install git
-fi
+	command -v git > /dev/null
+	if [ $? -ne 0 ]
+	then
+		sudo apt-get install git
+	fi
 
-git clone git@github.com:aliasbind/vimconfig.git ~/.vim
-if [ $? -ne 0 ]
-then
-	echo "Failed to install vim configuration."
-	exit 1
+	git clone --recursive git@github.com:aliasbind/vimconfig.git ~/.vim
+	if [ $? -ne 0 ]
+	then
+		echo "Failed to install vim configuration."
+		exit 1
+	fi
+	ln -s ~/.vim/vimrc ~/.vimrc
+	echo "vim: Success!"
+else
+	echo "vim: Configuration was not installed because you already have"
+	echo "a .vim directory in $HOME. Remove that directory and rerun"
+	echo "this script to install this configuration."
 fi
-ln -s ~/.vim/vimrc ~/.vimrc
