5e2a53191902b27447163722ca2897cdabd17d96
Andrei Gorgan <aliasbind@gmail.com>
2012-12-24 12:03:19 +0200

    Prettify installer script output

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ INSTALLER_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 I3_CONFIG_DIR=~/.i3
 mkdir -p $I3_CONFIG_DIR
 ln -fs $INSTALLER_PATH/i3/config $I3_CONFIG_DIR/config
-echo "i3: Success"
+echo -e "i3:\tSuccess"
 
 # dunst
 DUNST_CONFIG_DIR=~/.config/dunst
 mkdir -p $DUNST_CONFIG_DIR
 ln -fs $INSTALLER_PATH/dunst/dunstrc $DUNST_CONFIG_DIR/dunstrc
-echo "dunst: Success"
+echo -e "dunst:\tSuccess"
 
 # urxvt
 URXVT_CONFIG_DIR=~/.config
@@ -22,11 +22,10 @@ xrdb ~/.Xresources
 command -v xsel 2>&1 > /dev/null
 if [ $? -ne 0 ]
 then
-	echo "Need to install xsel"
 	sudo apt-get install -y xsel
 fi
 
-echo "urxvt: Success"
+echo -e "urxvt:\tSuccess"
 
 # vim
 if [ ! -d ~/.vim ]
@@ -34,17 +33,23 @@ then
 	git clone --recursive git@github.com:aliasbind/vimconfig.git ~/.vim
 	if [ $? -ne 0 ]
 	then
-		echo "Failed to install vim configuration."
+		echo -e "vim:\tFailed to install configuration directory."
 		exit 1
 	fi
 	ln -s ~/.vim/vimrc ~/.vimrc
-	echo "vim: Success!"
+	echo "vim:\tSuccess!"
 else
-	echo "vim: Configuration was not installed because you already have"
-	echo "a .vim directory in $HOME. Remove that directory and rerun"
-	echo "this script to install this configuration."
+	echo -e "vim:\tConfiguration was not installed because you already have"
+	echo -e "\ta .vim directory in $HOME. Remove that directory"
+	echo -e "\tand rerun this script to install this configuration."
 fi
 
 # zsh
 ln -fs $INSTALLER_PATH/zshrc ~/.zshrc
-git clone https://github.com/robbyrussel/oh-my-zsh.git ~/.oh-my-zsh
+if [ ! -d ~/.oh-my-zsh ]
+then
+	git clone https://github.com/robbyrussel/oh-my-zsh.git ~/.oh-my-zsh
+	echo -e "zsh:\tSuccess"
+else
+	echo -e "zsh:\tzshrc file added, but oh-my-zsh is already installed"
+fi
